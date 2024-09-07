bc50df3f692cea3ea86477dfc6f4c3185b138368
muzykalegion <muzyka.legion@gmail.com>
2024-09-07 20:13:45 +0300

    Add stand logs

--- log_viewer.py
+++ log_viewer.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filepath', help='Relative path fo a file, e.g. LOGS/27.05.24/1.log')
     args = parser.parse_args()
-    log_name = '2024-09-07--14-11-03'
+    log_name = '2024-09-07--18-08-16'
     filepath = args.filepath if args.filepath else f'LOGS/board/{log_name}.log'
     times, alts, throttles, pitches, cfg_line = parse_log(filepath)
 
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 
     ax[1].plot(mtimes, alts, ':b')
     ax[1].legend(['Altitude'])
-    ax[1].set_ylim(-0.5, 30)
+    ax[1].set_ylim(-0.5, 3.5)
 
     # ax[2].plot(mtimes, pitches, ':c')
     # ax[2].legend(['Pitch'])
