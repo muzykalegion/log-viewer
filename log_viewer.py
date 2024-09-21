b7e1f696bd468a037b4af4fec788bd6c343bcf92
muzykalegion <muzyka.legion@gmail.com>
2024-09-21 18:53:22 +0300

    Third round

--- log_viewer.py
+++ log_viewer.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filepath', help='Relative path fo a file, e.g. LOGS/27.05.24/1.log')
     args = parser.parse_args()
-    log_name = '2024-09-21--16-33-25'
+    log_name = '2024-09-21--18-13-26'
     filepath = args.filepath if args.filepath else f'LOGS/board/{log_name}.log'
     times, alts, throttles, pitches, cfg_line = parse_log(filepath)
 
