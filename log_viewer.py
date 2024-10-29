029e284bb2870ba740c0d8676fa0a6b5b1e533b8
muzykalegion <muzyka.legion@gmail.com>
2024-10-29 16:50:41 +0200

    Targeting logs

--- log_viewer.py
+++ log_viewer.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filepath', help='Relative path fo a file, e.g. LOGS/27.05.24/1.log')
     args = parser.parse_args()
-    log_name = '2024-10-22--12-43-06'
+    log_name = '2024-10-29--12-33-04'
     filepath = args.filepath if args.filepath else f'LOGS/board/{log_name}.log'
     times, alts, throttles, pitches, acc_zs, cfg_line = parse_log(filepath)
 
